3fe036aca963905cbdd339510ddf578cc62809e8
Marchabar <marchabar1@alum.us.es>
2023-05-20 21:27:26 +0200

    refactor

--- clusterComputing.py
+++ clusterComputing.py
@@ -10,29 +10,29 @@ from inputData import inputData
 # Function for recognizing the centers of the circles plotted in dataGenerator.py
 def recognizingCircles(allPoints, numberCirc,m = 1.1, maxIter=300, error=1e-5):
     
-    X = np.array(allPoints)
-    n = X.shape[0] # number of data points
-    d = X.shape[1] # number of coordinates of each point
+    initialPoints = np.array(allPoints)
+    numberDataPoints = initialPoints.shape[0] # number of data points
+    numberCoordinates = initialPoints.shape[1] # number of coordinates of each point
 
     # randomly initialize membership matrix
-    memberships = np.random.rand(n, numberCirc)
+    memberships = np.random.rand(numberDataPoints, numberCirc)
     # normalizing the membership matrix so each row sums 1
     memberships = memberships / np.sum(memberships, axis=1, keepdims=True)
         
     # iterate until convergence or max iterations
     for i in range(maxIter):
         # initialize centroids (row, col)
-        centroids = np.zeros((numberCirc, d))
+        centroids = np.zeros((numberCirc, numberCoordinates))
         # initialize distances (row, col)
-        distances = np.zeros((n, numberCirc))
+        distances = np.zeros((numberDataPoints, numberCirc))
         for k in range(numberCirc):
             # compute centroids for each circumference
             # numerator: sum of all the points to the power of m times the membership matrix column of that circumference
             # denominator: sum of the  membership matrix colummn for this circumference to the power of m 
-           centroids[k] = np.sum(memberships[:, k:k+1] ** m * X, axis=0) / np.sum(memberships[:, k:k+1] ** m)
+           centroids[k] = np.sum(memberships[:, k:k+1] ** m * initialPoints, axis=0) / np.sum(memberships[:, k:k+1] ** m)
            
-        # compute ecuclidean distances between each center and each point using cdist used in previous labs
-        distances = sc.cdist(X, centroids, metric='euclidean')
+        # compute ecuclidean distances between each center and each point using cdist 
+        distances = sc.cdist(initialPoints, centroids, metric='euclidean')
             
         
         radiusesCentroids = np.zeros(numberCirc)
